7f97d12ec9ef9ecbff44e3a5675e065a89d84362
Dragon-005 <66313627+Dragon-005@users.noreply.github.com>
2023-02-05 17:03:46 +0530

    Update view.py

--- view.py
+++ view.py
@@ -12,7 +12,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import mysql.connector as mc
 
 class Ui_view(object):
-    def setupUi(self, view):
+    def setupUi(self, view):                                                #Creating View Window
         view.setObjectName("view")
         view.resize(1129, 665)
         self.centralwidget = QtWidgets.QWidget(view)
